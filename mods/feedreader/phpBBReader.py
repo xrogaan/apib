2d8081ca127d8aab4154a3a44d40bc2e53fdb5a2
Ludovic Bellière <xrogaan@gmail.com>
2010-08-23 15:25:21 +0200

    Add a method the unescape htmlentities

--- mods/feedreader/phpBBReader.py
+++ mods/feedreader/phpBBReader.py
@@ -5,6 +5,36 @@ import feedparser
 import mods
 import time
 
+import re, htmlentitydefs
+
+def unescape(text):
+    """
+    Removes HTML or XML character references and entities from a text string.
+    Borrowed from http://effbot.org/zone/re-sub.htm#unescape-html
+
+    @param text The HTML (or XML) source text.
+    @return The plain text, as a Unicode string, if necessary
+    """
+    def fixup(m):
+        text = m.group(0)
+        if text[:2] == "&#":
+            # character reference
+            try:
+                if text[:3] == "&#x":
+                    return unichr(int(text[3:-1], 16))
+                else:
+                    return unichr(int(text[2:-1]))
+            except ValueError:
+                pass
+        else:
+            # named entity
+            try:
+                text = unichr(htmlentitydefs.name2codepoint[text[1:-1]])
+            except KeyError:
+                pass
+        return text # leave as is
+    return re.sub("&#?\w+;", fixup, text)
+
 class phpBBReader(mods.Plugin):
 
     lastId = None
@@ -58,10 +88,9 @@ class phpBBReader(mods.Plugin):
                                     }
             print verbose
             entries.append({
-                    'author':   entry.author.encode('UTF-8'),
-                    'category': entry.category.encode('UTF-8'),
-                    'link':     entry.link.encode('UTF-8'),
-                    'title':    entry.title.encode('UTF-8')
+                    'author':   entry.author,
+                    'link':     entry.link,
+                    'title':    entry.title
                    })
 
         if firstId != None:
@@ -78,12 +107,11 @@ class phpBBReader(mods.Plugin):
         if len(messages)==0:
             return msg
 
-        pattern = "[Forum] %s posted in %s " + unichr(8226) + " %s - %s "
+        pattern = "[Forum] %s posted in %s - %s"
         for p in messages:
-            msg.append(pattern % (p['author'].decode('UTF-8'),
-                                  p['category'].decode('UTF-8'),
-                                  p['title'].decode('UTF-8'),
-                                  p['link'].decode('UTF-8')))
+            msg.append(pattern % (p['author'],
+                                  unescape(p['title']),
+                                  p['link']))
         return msg
 
     def _get_bitly_url(self, url):
