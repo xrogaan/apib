f7c03b8d84bc8e7d5d4c74b6bddca731e9400ea7
Ludovic Bellière <xrogaan@users.sourceforge.net>
2010-12-31 14:37:04 +0100

    can now use 'special' factoids: load, add and save

--- mods/factoids/yamlAdapter.py
+++ mods/factoids/yamlAdapter.py
@@ -4,16 +4,27 @@
 import yaml
 import mods.Plugins
 
-class factoidsLoader(mods.PLugin):
+class factoidsLoader(mods.Plugin):
     factoidsFile = 'factoids.yaml'
 
     def __init__(self, shedulefn, args={}):
+        """
+        This will simply load the factoids. The yaml file must be in the proper
+        format for that. Extra test could be usefull.
+        """
         self._shedulefn = shedulefn
+        self._fSpecials = [
+                'add',
+                'save',
+                'load'
+        ]
 
+        self._load_factoids()
+
+    def _load_factoids(self):
         self.rawdata = yaml.load(open(self.factoidsFile, 'rb').read())
-        self.rawdata.update({
-            'add': "this command isn't implemented yet."
-        })
+        for fSpecial in self._fSpecial:
+            self.rawdata.update({fSpecial: "This command is special."})
 
     def _save_factoids(self):
         filestream = open(self.factoidsFile, 'w')
@@ -36,5 +47,30 @@ class factoidsLoader(mods.PLugin):
                 if self.body.startswith(':%s' % key):
                     c.privmsg(target, action)
                     return
+
+    def _do_special(self, body):
+        """
+        Call special action for special commands.
+        """
+        for fSpecial in self._fSpecial:
+            if body.startswith(':%s' % fSpecial):
+
+                fAttr = '_do_%s' % fSpecial
+
+                if not hasattr(self, fAttr):
+                    raise AttributeError(
+                        "Trying to call %s but doesn't exist." % fAttr
+                    )
+                else:
+                    getattr(self, fAttr)(body.split(None, 2))
+
+    def _do_save(self, nothing):
+        self._save_factoids()
+
+    def _do_load(self, nothing):
+        self._load_factoids()
+
+    def _do_add(self, data):
+        self.rawdata.update({data[2]: data[3]})
 
 Class=factoidsLoader
