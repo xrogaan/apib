50536a3782bc52e0e0753d9cd7ddea480f023f69
Ludovic Bellière <xrogaan@users.sourceforge.net>
2012-12-27 04:34:32 +0100

    typo s/message/messages/
    Who has two thumbs and remembers the rudiments of his linear algebra courses? Apparently, this guy.

--- mods/feedreader/phpBBReader.py
+++ mods/feedreader/phpBBReader.py
@@ -185,7 +185,7 @@ class phpBBReader(mods.Plugin):
 
         msg = []
 
-        if message ==1 or len(messages)==0:
+        if messages ==1 or len(messages)==0:
             return msg
 
         pattern = "[off][Forum] %(author)s posted in %(title)s - %(link)s"
